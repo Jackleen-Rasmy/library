9d1ddf740862f7a01344da0140d7a984768166fd
Jackleen Rasmy <jackleen.rasmy@gmail.com>
2023-11-18 22:05:47 +0200

    crud-for-book-model

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('books/', book_list , name='book-list'),
+    path('book/<int:book_id>', book_detail , name='book-detail'),
+    path('book/create/', CreateBook.as_view() , name='create-book'),
+    path('book/edit/<int:pk>', EditBook.as_view() , name='edit-book'),
+    path('book/delete/<int:pk>', DeleteBook.as_view() , name='edit-book'),
   
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import * 
+from django.views.generic import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    
+    return render(request, 'library/book_list.html' , {'books':books})
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    context = {'book':book}
+    return render(request, 'library/book_detail.html', context)
+
+
+class CreateBook(CreateView):
+    model = Book
+    fields = '__all__'                       
+    success_url = '/books/'
+    
+    
+class EditBook(UpdateView):
+    model = Book
+    fields = '__all__'
+    success_url = '/books/'
+    template_name = 'library/book_edit.html'
+    
+    
+class DeleteBook(DeleteView):
+    model = Book
+    success_url = '/books/'
+    
