e49ad6d703ab6ea5f582e5a1b4e3db1221212171
Jackleen Rasmy <jackleen.rasmy@gmail.com>
2023-11-18 22:18:18 +0200

    'change-path-name'

--- library/urls.py
+++ library/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('book/<int:book_id>', book_detail , name='book-detail'),
     path('book/create/', CreateBook.as_view() , name='create-book'),
     path('book/edit/<int:pk>', EditBook.as_view() , name='edit-book'),
-    path('book/delete/<int:pk>', DeleteBook.as_view() , name='edit-book'),
+    path('book/delete/<int:pk>', DeleteBook.as_view() , name='delete-book'),
   
 ]
